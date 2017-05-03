7fbe9559db024ce332397f099b7f592efeaa03c2
levan gulisashvili <levan.gulisashvili@gmail.com>
2017-05-03 19:47:09 +0300

    added offset to else statement

--- src/components/picker.js
+++ src/components/picker.js
@@ -275,7 +275,7 @@ export default class Picker extends React.Component {
           scroller.scrollTo(category.name, {
             duration: 300,
             smooth: true,
-            offset: 70
+            offset: -100
           })
 
         }
